4e4b73f8ff54062253b0a26378eb5512823ebbe6
github username <github email address>
2018-12-30 22:31:49 -0500

    added mailer now to spiff up my projects and add some visual bells and whistles to the site

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "./components/navbar";
-import Hero from "./components/hero";
-import AboutMe from "./components/aboutMe";
-import Skills from "./containers/skills";
-// import { reactIcon as ReactIcon } from "./components/icons/icons.js"
-// import SkillShelf from "./containers/skillShelf";
+import MainContent from "./containers/mainContent";
 import "./css/App.css";
 
 class App extends Component {
   render() {
     return (<>
       <Navbar/>
-      <Hero/>
-      <AboutMe>
-        <Skills/>
-      </AboutMe>
+      <MainContent/>
     </>);
   }
 }
--- src/copy/mainCopy.js
+++ src/copy/mainCopy.js
@@ -6,7 +6,7 @@ const MainCopy = {
 export const AboutMe = {
   intro: "So who am I?",
 
-  story: "I'm a JavaScript Full Stack Web Developer with a Bachelor's in Computer Animation and Interactive Design with 2 years of under grad Computer Science coursework completed. I made a few games for class and my thesis and the coding bug bit and hasn't let go since.",
+  story: "I'm a JavaScript Full Stack Web Developer with a Bachelor's in Computer Animation and Interactive Design with 2 years of under grad Computer Science coursework completed. I started out coding little games and playing with process.js and some node-based coding in Maya and the Unreal Engine. Eventually I had a class in Unity, most of the coding was done in C# and that's when the coding bug bit and it hasn't let go since then.",
   
   stack: ["GraphQL", "React", "Redux", "Node", "Express", "Postgres"]
 }
